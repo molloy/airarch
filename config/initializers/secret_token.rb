c8b81ba16db79be654265d9d010da46f272f97a6
Jeff Chang <molloy@gmail.com>
2011-08-22 23:45:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airarch::Application.config.secret_token = '984826de7dcfce03aed3aa37ee2fd3b976107b7c18f2cd599e44c658c7b9f251a806a2ba4988b7cefb8143df946aa7b0f8a92e08bd8e3f2590775becc5be16ba'
